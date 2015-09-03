61afea4b2b6652166c17767e3ea5dd8dd87266e6
Masahiro Sakai <masahiro.sakai@gmail.com>
2015-09-03 23:24:03 +0900

    add compatibility with slideshare plugin from tdiary-contrib

--- plugin/slideshare.rb
+++ plugin/slideshare.rb
@@ -1,4 +1,9 @@
 def slideshare(url)
-  url = CGI.escapeHTML(url)
-  %Q[<object type="application/x-shockwave-flash" data="#{url}" width="425" height="348"><param name="movie" value="#{url}" /></object>]
+  if /^http/ =~ url
+    url = CGI.escapeHTML(url)
+    %Q[<object type="application/x-shockwave-flash" data="#{url}" width="425" height="348"><param name="movie" value="#{url}" /></object>]
+  else
+    embed_code = url
+    %Q|<iframe src="http://www.slideshare.net/slideshow/embed_code/#{embed_code}" width="425" height="355" frameborder="0" marginwidth="0" marginheight="0" scrolling="no"></iframe>|
+  end
 end
